9194662d58d7873f17d567cc8ab40e24f830a3a8
SUBHAMYADAV\This PC <subhamyadav7528@gmail.com>
2022-10-01 16:19:32 +0530

    strat

---
+++ add.py
@@ -0,0 +1,12 @@
+#create two variables (a and b) and assign values to them.
+#create a third variable (sum) and add a and b.
+#print sum
+a=4
+b=7
+sum = a+b
+print(sum)
+
+x= input ("Enter first number :")
+y= input ("Enter second number")
+sum= int(x)+int(y)
+print("The answer is:",sum)
---
+++ practice.py
@@ -0,0 +1,5 @@
+x=5
+#below is to specify our own class like int can be changed to be shown as float or anything.
+
+a= float(x)
+print (type(a))
